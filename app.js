bac207a590fb04dab5a9f88761aa3c733aa469bf
im94 <ivan@vvm.agency>
2024-08-04 16:36:48 +0200

    updated app js file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const routerApi = require('./router-api');
-const apiPath = process.env.DB_HOST || '/api';
+const apiPath = process.env.CUSTOM_PATH || '/api';
 
 app.use(express.urlencoded({ extended: false })); // add user submited data (from form) to our request object
 app.use(express.json()); // send json data to our request object
