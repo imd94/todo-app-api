bceda7cc9ab7bc79b3d08836b84e9cd3a9064ebe
im94 <ivan@vvm.agency>
2024-08-04 18:13:39 +0200

    updated db js file

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 
 async function connectDB() {
   const connection = await mysql.createConnection({
-    host: process.env.DB_HOST || "127.0.0.1",
-    user: process.env.DB_USER || "todoappuser",
-    password: process.env.DB_PASSWORD || "2!9Ofq2d1S*$",
-    database: process.env.DB_NAME || "todoapp_db",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     port: 3306,
     multipleStatements: true
   });
