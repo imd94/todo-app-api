4bd8f594add9b48b450752e97be52004475bd0f1
im94 <ivan@vvm.agency>
2024-08-05 00:40:00 +0200

    debugging db connection

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const routerApi = require('./router-api');
-const apiPath = process.env.CUSTOM_PATH || '/api';
+const apiPath = process.env.CUSTOM_PATH;
 
 app.use(express.urlencoded({ extended: false })); // add user submited data (from form) to our request object
 app.use(express.json()); // send json data to our request object
